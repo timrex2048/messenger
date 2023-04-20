42d4d69c90839f1f3e7e055c37b2dd86448ded79
timrex <timwork2048z@gmail.com>
2023-04-20 20:15:34 +0300

    initial

--- client/run_client.py
+++ client/run_client.py
@@ -1,6 +1,7 @@
 """файл для запуска клиентского приложения в цикле"""
 from argparse import ArgumentParser
-from asyncio import ensure_future, get_event_loop, run, create_task, set_event_loop
+from asyncio import ensure_future, get_event_loop, run, create_task, \
+    set_event_loop
 from sys import argv
 
 from PyQt5.QtWidgets import QApplication, QDialog
@@ -8,7 +9,8 @@ from quamash import QEventLoop
 
 from client.utils.client_proto import ChatClientProtocol, ClientAuth
 from client.client_config import DB_PATH, PORT
-from client.ui.windows import LoginWindow
+from client.ui.windows import LoginWindow, ContactsWindow
+
 
 class ConsoleClientApp:
     """Console Client"""
@@ -22,11 +24,12 @@ class ConsoleClientApp:
         # create event loop
         loop = get_event_loop()
 
+
         # authentication process
         auth = ClientAuth(db_path=self.db_path)
         while True:
-            usr = self.args["user"] or input('username:')
-            passwrd = self.args["password"] or input('password:')
+            usr = self.args["user"] or input('username: ')
+            passwrd = self.args["password"] or input('password: ')
             auth.username = usr
             auth.password = passwrd
             is_auth = auth.authenticate()
@@ -40,10 +43,10 @@ class ConsoleClientApp:
                                      loop=loop,
                                      username=usr,
                                      password=passwrd)
+
         try:
             coro = loop.create_connection(lambda: client_, self.args["addr"],
                                           self.args["port"])
-
             transport, protocol = loop.run_until_complete(coro)
         except ConnectionRefusedError:
             print('Error. wrong server')
@@ -54,6 +57,7 @@ class ConsoleClientApp:
             tasks.append(task)
             loop.run_until_complete(task)
 
+
         except KeyboardInterrupt:
             pass
         except Exception as e:
@@ -62,23 +66,61 @@ class ConsoleClientApp:
         finally:
             loop.close()
 
+
 class GuiClientApp:
-    """GUI Client&quot"""
+    """GUI Client"""
+
     def __init__(self, parsed_args, db_path):
         self.args = parsed_args
         self.db_path = db_path
         self.ins = None
-    def main(self):
 
+    def main(self):
         # create event loop
         app = QApplication(argv)
         loop = QEventLoop(app)
-        set_event_loop(loop) # NEW must set the event loop
+        set_event_loop(loop)  # NEW must set the event loop
+
         # authentication process
-        login_wnd = LoginWindow()
+        auth_ = ClientAuth(db_path=self.db_path)
+        login_wnd = LoginWindow(auth_instance=auth_)
 
         if login_wnd.exec_() == QDialog.Accepted:
-            pass
+            # Each client will create a new protocol instance
+            client_ = ChatClientProtocol(db_path=self.db_path,
+                                         loop=loop,
+                                         username=login_wnd.username,
+                                         password=login_wnd.password)
+
+            # create Contacts window
+            wnd = ContactsWindow(client_instance=client_, user_name=login_wnd.username)
+            client_.gui_instance = wnd  # reference from protocol to GUI, for msg update
+
+            with loop:
+                # cleaning old instances
+                del auth_
+                del login_wnd
+
+                # connect to our server
+                try:
+                    coro = loop.create_connection(lambda: client_, self.args["addr"], self.args["port"])
+                    server = loop.run_until_complete(coro)
+                except ConnectionRefusedError:
+                    print('Error. wrong server')
+                    exit(1)
+
+                # start GUI client
+                wnd.show()
+                client_.get_from_gui()  # asyncio.ensure_future(client_.get_from_gui(loop))
+
+                # Serve requests until Ctrl+C
+                try:
+                    loop.run_forever()
+                except KeyboardInterrupt:
+                    pass
+                except Exception:
+                    pass
+
 
 def parse_and_run():
     def parse_args():
@@ -89,16 +131,19 @@ def parse_and_run():
         parser.add_argument("--port", default=PORT, type=int)
         parser.add_argument('--nogui', action='store_true')
         return vars(parser.parse_args())
+
     args = parse_args()
+
     if args['nogui']:
         # start consoles server
         a = ConsoleClientApp(args, DB_PATH)
         a.main()
-
     else:
         # start GUI client
         a = GuiClientApp(args, DB_PATH)
         a.main()
+
+
 
 if __name__ == '__main__':
     parse_and_run()
--- server/run_server.py
+++ server/run_server.py
@@ -1,10 +1,11 @@
-from argparse import ArgumentParser
-from asyncio import get_event_loop, set_event_loop
+"""файл для запуска серверного приложения в цикле"""
+import argparse
+import asyncio
+import sys
 
-from quamash import QEventLoop
-from PyQt5.QtWidgets import QApplication
-from sys import argv
-
+from PyQt5 import Qt
+#from PyQt5.QtCore import QEventLoop
+from quamash import QEventLoop  # asyncio works fine with pyqt5 loop
 
 from server.server_config import DB_PATH, PORT
 from server.utils.server_proto import ChatServerProtocol
@@ -12,6 +13,7 @@ from server.ui.windows import ServerMonitorWindow
 
 
 class ConsoleServerApp:
+    """Console server"""
 
     def __init__(self, parsed_args, db_path):
         self.args = parsed_args
@@ -21,10 +23,11 @@ class ConsoleServerApp:
     def main(self):
         connections = dict()
         users = dict()
-        loop = get_event_loop()
+        loop = asyncio.get_event_loop()
 
         # Each client will create a new protocol instance
         self.ins = ChatServerProtocol(self.db_path, connections, users)
+
         coro = loop.create_server(lambda: self.ins, self.args["addr"], self.args["port"])
         server = loop.run_until_complete(coro)
 
@@ -34,17 +37,20 @@ class ConsoleServerApp:
             loop.run_forever()
         except KeyboardInterrupt:
             pass
+
         server.close()
         loop.run_until_complete(server.wait_closed())
         loop.close()
 
 
 class GuiServerApp:
-    """Gui server&quot"""
+    """Gui server"""
+
     def __init__(self, parsed_args, db_path):
         self.args = parsed_args
         self.db_path = db_path
         self.ins = None
+
     def main(self):
         connections = dict()
         users = dict()
@@ -53,29 +59,32 @@ class GuiServerApp:
         self.ins = ChatServerProtocol(self.db_path, connections, users)
 
         # GUI
-        app = QApplication(argv)
+        app = Qt.QApplication(sys.argv)
         loop = QEventLoop(app)
-        set_event_loop(loop) # NEW must set the event loop
-        # server_instance=self.ins, parsed_args=self.args
-        wnd = ServerMonitorWindow()
+        asyncio.set_event_loop(loop)  # NEW must set the event loop
+
+        wnd = ServerMonitorWindow(server_instance=self.ins, parsed_args=self.args)
         wnd.show()
 
         with loop:
             coro = loop.create_server(lambda: self.ins, self.args["addr"], self.args["port"])
             server = loop.run_until_complete(coro)
+
             # Serve requests until Ctrl+C
             print('Serving on {}:{}'.format(*server.sockets[0].getsockname()))
             try:
                 loop.run_forever()
             except KeyboardInterrupt:
                 pass
+
             server.close()
             loop.run_until_complete(server.wait_closed())
             loop.close()
 
+
 def parse_and_run():
     def parse_args():
-        parser = ArgumentParser(description="Server settings" )
+        parser = argparse.ArgumentParser(description="Server settings")
         parser.add_argument("--addr", default="127.0.0.1", type=str)
         parser.add_argument("--port", default=PORT, type=int)
         parser.add_argument('--nogui', action='store_true')
@@ -87,11 +96,11 @@ def parse_and_run():
         # start consoles server
         a = ConsoleServerApp(args, DB_PATH)
         a.main()
-
     else:
         # start GUI server
         a = GuiServerApp(args, DB_PATH)
         a.main()
 
-if __name__=='__main':
+
+if __name__ == '__main__':
     parse_and_run()
